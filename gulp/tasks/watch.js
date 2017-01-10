14cb8ff331a5fabe3ade342910618b03237d19f6
heklaweb <heklaweb@t-online.de>
2017-01-10 20:09:51 +0100

    Completed large hero block and intro content block

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
-// Überwachung
+/* Überwachung */
 gulp.task('watch', function () {
 
     browserSync.init({
@@ -12,13 +12,13 @@ gulp.task('watch', function () {
         }
     });
 
-    // Änderungen an "index.html" überwachen
+    /* Änderungen an "index.html" überwachen */
     watch('./app/index.html', function () {
         // gulp.start('html');
         browserSync.reload();
     });
 
-    // Änderungen an *.css Dateien im "styles"-Verzeichnis überwachen
+    /* Änderungen an *.css Dateien im "styles"-Verzeichnis überwachen */
     watch('./app/assets/styles/**/*.css', function () {
         // gulp.start('styles');
         gulp.start('cssInject');
@@ -26,7 +26,7 @@ gulp.task('watch', function () {
 
 });
 
-// Änderungen an css-Dateien injizieren (ohne Reload!)
+/* Änderungen an css-Dateien injizieren (ohne Reload!) */
 gulp.task('cssInject', ['styles'], function () {
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
