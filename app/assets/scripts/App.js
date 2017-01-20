2ecc3796405ac1ddd5b5d97b26deba68e56f1419
heklaweb <heklaweb@t-online.de>
2017-01-21 00:43:15 +0100

    Revealing Elements on Scroll.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import MobilMenu from "./modules/MobilMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from 'jquery';
 
 var mobilMenu = new MobilMenu();
-
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
