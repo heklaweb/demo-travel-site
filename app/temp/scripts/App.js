c6bfe725176a6ddef127937871fc735304c520bc
heklaweb <heklaweb@t-online.de>
2017-01-28 11:05:16 +0100

    Completed lazy loading of images.

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11205,6 +11205,7 @@
 	    function StickyHeader() {
 	        _classCallCheck(this, StickyHeader);
 
+	        this.lazyImages = (0, _jquery2.default)(".lazyload"); /** wird ggf. nicht (mehr) benötigt */
 	        this.siteHeader = (0, _jquery2.default)(".site-header");
 	        this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
 	        this.createHeaderWaypoint();
@@ -11212,9 +11213,18 @@
 	        this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 	        this.createPageSectionWaypoints();
 	        this.addSmoothScrolling();
+	        this.refreshWaypoints(); /** wird ggf. nicht (mehr) benötigt */
 	    }
 
 	    _createClass(StickyHeader, [{
+	        key: 'refreshWaypoints',
+	        value: function refreshWaypoints() {
+	            /** wird ggf. nicht (mehr) benötigt */
+	            this.lazyImages.on("load", function () {
+	                Waypoint.refreshAll();
+	            });
+	        }
+	    }, {
 	        key: 'addSmoothScrolling',
 	        value: function addSmoothScrolling() {
 	            this.headerLinks.smoothScroll();
